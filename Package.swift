4312313864ba8ba699fd5e6505083e6d02ffb80a
Irwan Cannady <irwancannadys@gmail.com>
2026-06-06 12:16:06 +0700

    chore: update Package.swift with dev-latest checksum

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "SharedKMP",
             url: "https://github.com/SawitPro/MobileKmpApp-spm/releases/download/dev-latest/SharedKMP.xcframework.zip",
-            checksum: "placeholder"
+            checksum: "14eadb04e0d972f95c7fcbea57447bbd90aa3ebfcf817516635a3fa8a321e712"
         ),
     ]
 )
